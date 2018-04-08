310cf15bfb07c27188f281c621a43d2510efbd37
Suresh Yellamelli <ysuresh81@gmail.com>
2018-04-08 08:18:01 +0530

    Signed-off-by: Suresh Yellamelli <ysuresh81@gmail.com>

--- src/main/java/com/example/rest/webservices/demorestfulwebservice/user/UserDetailsDAO.java
+++ src/main/java/com/example/rest/webservices/demorestfulwebservice/user/UserDetailsDAO.java
@@ -2,6 +2,7 @@ package com.example.rest.webservices.demorestfulwebservice.user;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
@@ -15,9 +16,11 @@ public class UserDetailsDAO {
 		users.add(new User(2, "sample", new Date()));
 		users.add(new User(3, "test", new Date()));
 	}
+
 	public List<User> findAll() {
 		return users;
 	}
+
 	public User save(User user) {
 		if (user.getNumber() == null) {
 			user.setNumber(++counter);
@@ -26,9 +29,22 @@ public class UserDetailsDAO {
 		return user;
 
 	}
+
 	public User findOne(int id) {
 		for (User user : users) {
 			if (user.getNumber() == id) {
+				return user;
+			}
+		}
+		return null;
+	}
+
+	public User deleteById(int id) {
+		Iterator<User> iterator = users.iterator();
+		while (iterator.hasNext()) {
+			User user = iterator.next();
+			if (user.getNumber() == id) {
+				iterator.remove();
 				return user;
 			}
 		}
--- src/main/java/com/example/rest/webservices/demorestfulwebservice/user/UserResource.java
+++ src/main/java/com/example/rest/webservices/demorestfulwebservice/user/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +26,12 @@ public class UserResource {
 
 	@GetMapping("/users/{id}")
 	public User findUser(@PathVariable int id) {
-		return userdao.findOne(id);
+		User user = userdao.findOne(id);
+		
+		if(null == user)
+			throw new UserNotFoundException(String.format("ID %s not found",id));
+		
+		return user;
 	}
 
 	@PostMapping("/users")
@@ -37,4 +43,11 @@ public class UserResource {
 		return ResponseEntity.created(location).build();
 	}
 
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user = userdao.deleteById(id);
+		if(null == user)
+			throw new UserNotFoundException(String.format("ID %s not found",id));
+		
+	}
 }
